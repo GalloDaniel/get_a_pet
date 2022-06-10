eb123bf45122a0cce0367d81e26ed44bbe6f8e0a
GalloDaniel <danielcostagallo@gmail.com>
2022-06-09 22:01:19 -0300

    add front-end project

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+/* components */
+import Navbar from './components/layout/Navbar';
+import Footer from './components/layout/Footer';
+import Container from './components/layout/Container'
+import Message from './components/layout/Message';
+
+/* pages */
+import Home from './components/pages/Home'
+import Login from './components/pages/Auth/Login'
+import Register from './components/pages/Auth/Register'
+import Profile from './components/pages/User/Profile';
+
+/* context */
+import { UserProvider } from './context/UserContext';
+
+function App() {
+  return (
+    <Router>
+      <UserProvider>
+        <Navbar />
+        <Message />
+        <Container>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/user/profile" element={<Profile />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Container>
+        <Footer />
+      </UserProvider>
+    </Router>
+  );
+}
+
+export default App;
