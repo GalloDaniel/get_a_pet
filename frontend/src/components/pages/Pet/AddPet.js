4731d12e950ace59538f1529205a1ec5479656b0
GalloDaniel <danielcostagallo@gmail.com>
2022-06-13 15:06:16 -0300

    feat: add pet things

---
+++ frontend/src/components/pages/Pet/AddPet.js
@@ -0,0 +1,60 @@
+import api from '../../../utils/api'
+
+import styles from './AddPet.module.css'
+
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import PetForm from '../../form/PetForm'
+
+import useFlashMessage from '../../../hooks/useFlashMessage'
+
+function AddPet() {
+  const [token] = useState(localStorage.getItem('token') || '')
+  const { setFlashMessage } = useFlashMessage()
+  const navigate = useNavigate()
+
+  async function registerPet(pet) {
+    let msgType = 'success'
+    let msgText = ''
+
+    const formData = new FormData()
+
+    await Object.keys(pet).forEach(key => {
+      if (key === 'images') {
+        for (let i = 0; i < pet[key].length; i++) {
+          formData.append('images', pet[key][i])
+        }
+      } else {
+        formData.append(key, pet[key])
+      }
+    })
+
+    try {
+      const { data } = await api.post('pets/create', formData, {
+        Authorization: `Bearer ${JSON.parse(token)}`,
+        'Content-type': 'multipart/form-data'
+      })
+      msgText = data.message
+    } catch (error) {
+      msgType = 'error'
+      msgText = error.response.data.message
+    }
+
+    setFlashMessage(msgText, msgType)
+
+    if (msgType !== 'error') navigate('/pets/mypets')
+  }
+
+  return (
+    <section className={styles.addpet_header}>
+      <div>
+        <h1>Cadastre um Pet</h1>
+        <p>Depois ele ficará disponível para adoção</p>
+      </div>
+      <PetForm handleSumit={registerPet} btnText='Cadastrar Pet' />
+    </section>
+  )
+}
+
+export default AddPet
